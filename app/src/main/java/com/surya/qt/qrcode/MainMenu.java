c27338bd5cf2cde18537dc593961cccb1c6e9103
Evans Jahja <evans.jahja@yahoo.com>
2017-08-27 19:23:25 +0700

    JSON returns, intents viewlist

--- app/src/main/java/com/surya/qt/qrcode/MainMenu.java
+++ app/src/main/java/com/surya/qt/qrcode/MainMenu.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialog;
+import android.util.JsonReader;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +19,10 @@ import com.plasmagrass.promiseinternet.IDelegate;
 import com.plasmagrass.promiseinternet.Promise;
 import com.plasmagrass.promiseinternet.PromiseInternet;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
 public class MainMenu extends AppCompatActivity implements View.OnClickListener{
 
     @Override
@@ -25,7 +30,16 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Button scanBtn = (Button)findViewById(R.id.scanBtn);
+        Button viewListBtn = (Button)findViewById(R.id.viewListButton);
         scanBtn.setOnClickListener(this);
+        viewListBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //new Intent()
+                Intent i = new Intent(getApplicationContext(), ViewList.class);
+                startActivity(i);
+            }
+        });
 
     }
 
@@ -53,20 +67,24 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener{
             p.then(new IDelegate() {
                 @Override
                 public void execute(String s) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(s);
+                        String name = jsonObject.getString("name");
+                        String text = "Welcome, " + name;
 
-                    String nama = s;
-                    String text = "Welcome, " + nama;
-
-                    AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
-                    alert
-                            .setMessage(text)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .show();
+                        AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                        alert
+                                .setMessage(text)
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             });
 
